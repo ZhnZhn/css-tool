552ece1e737bfbeb5f089d3b350ea5a6a56f3d74
Yevhenii Molodyi <eugmolody@gmail.com>
2021-06-23 12:55:20 +0300

    Replace InputText by TypeScript implementation

--- src/components/types.ts
+++ src/components/types.ts
@@ -3,11 +3,15 @@ import type { CSSProperties as CSSProps } from 'react';
 export type CSSProperties = CSSProps
 export type StyleSheet<T extends string> = Record<T, CSSProperties> 
 
+export type { MouseEvent, KeyboardEvent, ChangeEvent } from 'react'
+
+export type { RefObject } from 'preact'
+
 type VElemType = JSX.Element | null 
 export type ChildrenType = VElemType
 export type FC<T, IS = true> = IS extends true 
   ? { (props: T & { children?: ChildrenType }): VElemType} 
   : { (props: T): VElemType }
 
-export type { MouseEvent, KeyboardEvent } from 'react'
+
 
---
+++ src/components/zhn/InputText.tsx
@@ -0,0 +1,107 @@
+import type { FC, CSSProperties, RefObject, ChangeEvent, KeyboardEvent } from '../types';
+
+import hooks from '../hooks';
+
+import crId from '../../utils/crId';
+
+const {
+  useState, useRef, useCallback,
+  useEffect, useImperativeHandle
+} = hooks;
+
+const CL_INPUT_TEXT = 'input-text box-shadow';
+
+type ValueType = string | number
+type InnerRefType = {
+  setValue: (initValue: ValueType) => void
+}
+
+interface InputTextProps {
+  style?: CSSProperties;
+  type?: 'text' | 'number';
+  name?: string;
+  initValue?: ValueType;
+  inputId?: string;
+  step?: number;
+  min?: number;
+  max?: number;
+  innerRef?: RefObject<InnerRefType>;
+  onChange?: (value: ValueType) => void;
+  onEnter?: (value: ValueType) => void;
+}
+
+
+type DfTextPropsType = {
+  autoCorrect: 'on' | 'off';
+  autoCapitalize: 'off' | 'none' | 'on' | 'sentences' | 'words' | 'characters';
+  spellCheck: 'true' | 'false';
+  translate: 'no' | 'yes' | undefined;
+  maxLength: number;
+}
+
+const DF_TEXT_PROPS: DfTextPropsType = {
+  autoCorrect: "off",
+  autoCapitalize: "off",
+  spellCheck: 'false',
+  translate: "no",
+  maxLength: 25
+};
+
+const _fnNoop = () => {}
+
+const InputText: FC<InputTextProps, false> = ({
+  style,
+  type='text',
+  name,
+  initValue='',
+  inputId,
+  step=1,
+  min=-10,
+  max=10,
+  innerRef,
+  onChange=_fnNoop,
+  onEnter=_fnNoop
+}) => {
+  const [value, setValue] = useState<ValueType>(initValue)
+  , _refName = useRef<string>(name || crId())
+  , _hInputChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+      const value = event.currentTarget.value;
+      setValue(value)
+      onChange(value)
+    }, [])
+  , _hKeyDown = useCallback((event: KeyboardEvent<HTMLInputElement>) => {
+      if (event.keyCode === 13){                
+        onEnter(event.currentTarget.value)
+      }
+    }, []);
+
+  useEffect(() => {
+    setValue(initValue)
+  }, [inputId])
+
+  useImperativeHandle(innerRef!, () => ({
+    setValue
+  }))
+
+  const  _textProps = type === 'text'
+    ? DF_TEXT_PROPS : void 0
+  , _numberProps = type === 'number'
+    ? { min, max, step } : void 0;
+
+  return (
+    <input
+      type={type}
+      name={_refName.current}
+      {..._textProps}
+      {..._numberProps}
+      className={CL_INPUT_TEXT}
+      style={style}
+      value={value}
+      //onChange={_hInputChange}
+      onInput={_hInputChange}
+      onKeyDown={_hKeyDown}
+    />
+  );
+};
+
+export default InputText
