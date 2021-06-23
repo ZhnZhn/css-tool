327a75556d22587ffda4b7f486e8549d05a20996
Yevhenii Molodyi <eugmolody@gmail.com>
2021-06-23 11:14:12 +0300

    Replace Color function component by TypeScript implementation

---
+++ src/components/zhn/Color.tsx
@@ -0,0 +1,25 @@
+import type { FC, CSSProperties } from '../types';
+
+interface ColorProps {
+  style: CSSProperties;
+  value: string;
+}
+
+const CL_BOX_COLOR = 'box-color box-shadow';
+
+const Color: FC<ColorProps, false> = ({ 
+  style, 
+  value 
+}) => {
+  const _style = value
+    ? { backgroundColor: value }
+    : null;
+  return (
+    <span 
+      className={CL_BOX_COLOR} 
+      style={{...style, ..._style}} 
+    />
+  );
+}
+
+export default Color
