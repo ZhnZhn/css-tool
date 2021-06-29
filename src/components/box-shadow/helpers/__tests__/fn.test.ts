8e753e317ce9dce76ea590759a19d5e8da648115
Yevhenii Molodyi <eugmolody@gmail.com>
2021-06-29 17:35:26 +0300

    Add unit tests for box shadow fn helpers

---
+++ src/components/box-shadow/helpers/__tests__/fn.test.ts
@@ -0,0 +1,28 @@
+import type { ShadowType } from '../../types';
+import fn from '../fn';
+
+const { 
+ toRgba,
+ toCssValue 
+} = fn;
+
+const _boxShadow: ShadowType = {
+   isInset: false,
+   gLength: 6,
+   vLength: 10,
+   blurR: 4,
+   spreadR: 5,
+   color: '#a8a9aa',
+   opacity: 0,
+   id: 'test'
+};
+
+describe("box-shadow helper fns", ()=>{
+   test("toRgba should return rgba start string from ShadowType", ()=>{     
+     expect(toRgba(_boxShadow)).toBe('rgba(168, 169, 170,')
+   }) 
+   test("toCssValue should return css shadow value string from ShadowType", ()=>{
+      expect(toCssValue(_boxShadow)).toBe('6px 10px 4px 5px rgba(168, 169, 170, 0)')
+      expect(toCssValue({..._boxShadow, isInset: true})).toBe('inset 6px 10px 4px 5px rgba(168, 169, 170, 0)')
+   })
+})
