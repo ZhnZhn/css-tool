ae34463ddfed10b9a999450d27b73c088de6063c
Yevhenii Molodyi <eugmolody@gmail.com>
2022-04-22 16:17:07 +0300

    Add HSL color input option to RowInputType3

--- src/components/zhn-r/RowInputType3.tsx
+++ src/components/zhn-r/RowInputType3.tsx
@@ -1,17 +1,25 @@
 import type { 
   CSSProperties, 
   FC,
-  TinycolorInstance 
+  StateUpdater,
+  TinycolorInstance,
+  HSLA 
 } from '../types';
 
 import tinycolor from '../../tinycolor/tinycolor';
 import { 
-  useState, 
-  useCallback 
+  useRef,
+  useState,
+  useMemo 
 } from '../uiApi';
+import useRefInit from '../hooks/useRefInit';
+import useToggle from '../hooks/useToggle';
+
 
 import InputText from '../zhn/InputText';
 import Color from '../zhn/Color';
+import ShowHide from '../zhn/ShowHide';
+import InputNumber from '../zhn/InputNumber';
 
 import {
   CL_ROW,
@@ -19,6 +27,17 @@ import {
   S_TEXT,
   S_COLOR
 } from './style';
+
+const S_HSL: CSSProperties = { 
+  paddingTop: 12,
+  paddingBottom: 12,
+  borderBottom: '1px solid rgba(0, 0, 0, 0.2)' 
+}
+, S_HSL_CAPTION: CSSProperties = { 
+  color: 'rgba(0, 0, 0, 0.5)',
+  paddingRight: 14
+}
+, S_INPUT_NUMBER: CSSProperties = { width: 50 };
 
 export interface RowInputType3Props {  
   id?: string;
@@ -31,6 +50,21 @@ export interface RowInputType3Props {
 
 const _FN_NOOP = () => {};
 
+const _fChangeItem = (
+  propName: 'h' | 's' | 'l', 
+  refHsl: { current: HSLA },
+  onEnter: (value: string, color: TinycolorInstance) => void, 
+  setValue: StateUpdater<string>
+  ) => (value: number) => {
+  refHsl.current[propName] = value  
+  const _color = tinycolor(refHsl.current) as TinycolorInstance;
+  if (_color && _color.isValid()){
+    const _value = _color.toHexString()      
+    onEnter(_value, _color)
+    setValue(_value)
+  }
+}
+
 const RowInputType3: FC<RowInputType3Props, false> = ({   
   id,
   styleInput,
@@ -39,14 +73,50 @@ const RowInputType3: FC<RowInputType3Props, false> = ({
   initValue,
   onEnter=_FN_NOOP
 }) => {
-  const [value, setValue] = useState(initValue)  
-  , _hEnter = useCallback((value: string) => {
-    const color = tinycolor(value) as TinycolorInstance;
+  const _refHex = useRef() 
+  , _refH = useRef()
+  , _refS = useRef()
+  , _refL = useRef()
+  , _refHsl = useRefInit(() => {
+    const _color = tinycolor(initValue) as TinycolorInstance;    
+    return _color.toHsl();
+  })
+  , [
+    isHsl, 
+    toggleIsHsl
+  ] = useToggle()
+  , [
+    value, 
+    setValue
+  ] = useState(initValue) 
+  , _hEnter = useMemo(() => (value: string) => {
+    const color = tinycolor(value) as TinycolorInstance;     
      if (color && color.isValid()){
+       const hsl = color.toHsl();
+       _refH.current?.setValue(hsl.h)
+       _refS.current?.setValue(hsl.s)
+       _refL.current?.setValue(hsl.l)
+       _refHsl.curent = hsl       
        onEnter(value, color)
-       setValue(color.toHexString())
+       setValue( color.toHexString())
      }
-  }, [onEnter]);  
+  }, [onEnter])
+  // _refHsl
+  , [
+     _hChangeH,
+     _hChangeS,
+     _hChangeL
+  ] = useMemo(() => [
+    _fChangeItem('h', _refHsl, onEnter, setValue),
+    _fChangeItem('s', _refHsl, onEnter, setValue),
+    _fChangeItem('l', _refHsl, onEnter, setValue)
+  ], [onEnter])
+  // _refHsl
+  , _hEnterHslColor = useMemo(() => () => {
+       _refHex.current?.setValue(value)
+  }, [value])  
+  , { h, s, l } = useMemo(() => _refHsl.current, [])
+  // _refHsl.current
 
   return (
    <div className={CL_ROW}>
@@ -54,6 +124,7 @@ const RowInputType3: FC<RowInputType3Props, false> = ({
       <label className={CL_CAPTION}>
         <span>{caption}</span>
         <InputText
+           innerRef={_refHex}
            key={inputId}
            id={id}
            style={{...S_TEXT, ...styleInput}}                      
@@ -63,9 +134,42 @@ const RowInputType3: FC<RowInputType3Props, false> = ({
          <Color
            style={S_COLOR}
            value={value}
+           onClick={toggleIsHsl}
          />
       </label>
       {/*eslint-enable jsx-a11y/label-has-for*/}
+      <ShowHide is={isHsl}>
+        <div style={S_HSL}>
+         <span style={S_HSL_CAPTION}>HSL</span>
+         <InputNumber
+           innerRef={_refH}
+           style={S_INPUT_NUMBER} 
+           initialValue={h}
+           min={0}
+           max={360}
+           onChange={_hChangeH}
+           onEnter={_hEnterHslColor}
+         />
+         <InputNumber 
+           innerRef={_refS}          
+           style={S_INPUT_NUMBER} 
+           initialValue={s}
+           min={0}
+           max={100}
+           onChange={_hChangeS}
+           onEnter={_hEnterHslColor}
+         />
+         <InputNumber     
+           innerRef={_refL}      
+           style={S_INPUT_NUMBER} 
+           initialValue={l}
+           min={0}
+           max={100}
+           onChange={_hChangeL}
+           onEnter={_hEnterHslColor}
+         />         
+        </div>
+      </ShowHide>
     </div>
   );
 }
