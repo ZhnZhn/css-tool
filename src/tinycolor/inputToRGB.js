7812b5ea49a3532b949f812ac771d963ce3d63f0
Yevhenii Molodyi <eugmolody@gmail.com>
2022-04-22 16:09:56 +0300

    Edit inputToRGB

--- src/tinycolor/inputToRGB.js
+++ src/tinycolor/inputToRGB.js
@@ -10,7 +10,12 @@ import {
 
 const mathMin = Math.min
 , mathMax = Math.max
-, _hasOwnProperty = Object.prototype.hasOwnProperty;
+, _hasOwnProperty = Object.prototype.hasOwnProperty
+, _isRgbFormat = color => isValidCSSUnit(color.r) 
+    && isValidCSSUnit(color.g) 
+    && isValidCSSUnit(color.b)
+, _isHsFormat = color => isValidCSSUnit(color.h) 
+   && isValidCSSUnit(color.s);
 
 function inputToRGB(color) {
    let rgb = { r: 0, g: 0, b: 0 }    
@@ -24,25 +29,28 @@ function inputToRGB(color) {
       color = stringInputToObject(color);
    }   
    if (typeof color == "object") {
-       if (isValidCSSUnit(color.r) && isValidCSSUnit(color.g) && isValidCSSUnit(color.b)) {
+       if (_isRgbFormat(color)) {
           rgb = rgbToRgb(color.r, color.g, color.b);
           ok = true;
-          format = String(color.r).substr(-1) === "%" ? "prgb" : "rgb";
+          format = String(color.r).substring(-1) === "%" ? "prgb" : "rgb";
        }
-       else if (isValidCSSUnit(color.h) && isValidCSSUnit(color.s) && isValidCSSUnit(color.v)) {
-          s = convertToPercentage(color.s);
-          v = convertToPercentage(color.v);
-          rgb = hsvToRgb(color.h, s, v);
-          ok = true;
-          format = "hsv";
+       else if (_isHsFormat(color)) {
+          if (isValidCSSUnit(color.v)) {
+            s = convertToPercentage(color.s);
+            v = convertToPercentage(color.v);
+            rgb = hsvToRgb(color.h, s, v);
+            ok = true;
+            format = "hsv";
+          } 
+          else if (isValidCSSUnit(color.l)) {
+            s = convertToPercentage(color.s);
+            l = convertToPercentage(color.l);
+            rgb = hslToRgb(color.h, s, l);
+            ok = true;
+            format = "hsl";
+          }
        }
-       else if (isValidCSSUnit(color.h) && isValidCSSUnit(color.s) && isValidCSSUnit(color.l)) {
-          s = convertToPercentage(color.s);
-          l = convertToPercentage(color.l);
-          rgb = hslToRgb(color.h, s, l);
-          ok = true;
-          format = "hsl";
-       }   
+          
        //if (color.hasOwnProperty("a")) {
        if (_hasOwnProperty.call(color, "a")) {    
          a = color.a;
