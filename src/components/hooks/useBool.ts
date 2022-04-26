f6dd6752e02ba4777e3841f68be1f69ffd622503
Yevhenii Molodyi <eugmolody@gmail.com>
2022-04-26 17:18:22 +0300

    Add useBool

---
+++ src/components/hooks/useBool.ts
@@ -0,0 +1,18 @@
+import { useState, useMemo } from '../uiApi';
+
+const useBool = (initialValue: any) => {
+  const [
+    is, 
+    setIs
+  ] = useState(() => !!initialValue)  
+  , [ 
+    setTrue, 
+    setFalse
+  ] = useMemo(() => [
+    () => setIs(true),
+    () => setIs(false)
+  ], []);      
+  return [is, setTrue, setFalse];
+};
+
+export default useBool
