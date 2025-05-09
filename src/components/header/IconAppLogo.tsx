2a770f26fce35fe876285f27a5b316d918df8a9e
Yevhenii Molodyi <eugmolody@gmail.com>
2025-05-09 13:13:09 +0300

    Improve IconAppLogo function component

--- src/components/header/IconAppLogo.tsx
+++ src/components/header/IconAppLogo.tsx
@@ -5,6 +5,26 @@ interface IconAppLogoProps {
   title: string;
 }
 
+const _crRect = (
+  rx: number,
+  y: number, 
+  x: number,
+  height: number, 
+  width: number,
+  color: string
+) => (
+  <rect
+    ry="2"
+    rx={rx}
+    y={y}
+    x={x}
+    height={height}
+    width={width}
+    fill={color}
+    stroke={color}
+  />
+);
+
 const IconAppLogo: FC<IconAppLogoProps, false> = ({ 
   className, 
   title 
@@ -21,36 +41,9 @@ const IconAppLogo: FC<IconAppLogoProps, false> = ({
       stroke-width="2"
     >
       <title>{title}</title>
-      <rect
-        ry="1.8825414"
-        rx="194.14471"
-        y="1.434558"
-        x="19.176462"
-        height="12.460618"
-        width="10.85316"
-        fill="#8ecc2d"
-        stroke="#8ecc2d"
-      />
-      <rect
-        ry="1.87537"
-        rx="204.478"
-        y="16.639841"
-        x="8.8410215"
-        height="13.752699"
-        width="18.307165"
-        fill="#232f3b"
-        stroke="#232f3b"
-      />
-      <rect
-        ry="1.87537"
-        rx="204.478"
-        y="3.4229634"
-        x="2.5897937"
-        height="10.894996"
-        width="10.001963"
-        fill="#a487d4"
-        stroke="#a487d4"
-      />
+      {_crRect(194, 1.5, 19, 12.5, 11, "#8ecc2d")}
+      {_crRect(204.5, 17.5, 9, 13.5, 18, "#232f3b")}
+      {_crRect(204.5, 3.5, 2.5, 11, 10, "#a487d4")}      
     </svg>
   </span>
 );
