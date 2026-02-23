bb91e17a8a9fa54f67b59f7f401a31f4b7edc90b
Yevhenii Molodyi <eugmolody@gmail.com>
2026-02-23 16:48:02 +0200

    Edit eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -9,11 +9,12 @@ const jsxA11y = require("eslint-plugin-jsx-a11y");
 
 module.exports = defineConfig([ 
   tseslint.configs.strict,  
-  jsxA11y.flatConfigs.recommended, {
+  jsxA11y.flatConfigs.recommended, {  
   ignores: [
     "js/*",
     "node_modules/*"   
   ],
+
   languageOptions: {
     globals: {
       document: true,
@@ -44,8 +45,6 @@ module.exports = defineConfig([
      
   rules: {
     //edit eslint rules
-    "for-direction": 0,
-
     "no-mixed-spaces-and-tabs": [2, "smart-tabs"],
     "no-console": 0,
     "no-whitespace-before-property": 2,
@@ -56,37 +55,17 @@ module.exports = defineConfig([
     "no-unused-vars": [1, {"args": "none"}],
     "no-use-before-define": ["error", { "variables": false }],
    
-    //set react rules
-    "react/display-name": 0,
-    "react/jsx-boolean-value": [1, "always"],
-    "react/jsx-no-undef": 0,
-
-    "react/jsx-sort-prop-types": 0,
-    "react/jsx-sort-props": 0,
-    "react/jsx-uses-react": 1,
+    //set react rules   
+    "react/jsx-boolean-value": [1, "always"],    
     "react/jsx-uses-vars": 1,
-
-    "react/no-did-mount-set-state": 0,
-    "react/no-did-update-set-state": 0,
-
-    "react/no-multi-comp": 0,
-    "react/no-unknown-property": 0,
-
-    "react/prop-types": 0,
-    "react/react-in-jsx-scope": 1,
     "react/self-closing-comp": 1,
 
-    "react/sort-comp": 0,
     "react/jsx-wrap-multilines": ["error", {
        "declaration": true,
        "assignment": true,
        "return": true,
        "arrow": true
     }],
-
-    //jsx-transform
-    "react/jsx-uses-react": "off",
-    "react/react-in-jsx-scope": "off",
 
     //set react-hooks rules
     "react-hooks/rules-of-hooks": "error",
