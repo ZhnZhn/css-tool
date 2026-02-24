bbe4a57a1277447dbb7fdfc3e02bb2a29b242de8
Yevhenii Molodyi <eugmolody@gmail.com>
2026-02-24 14:24:01 +0200

    Replace props destructuring by props for function component RowInputText

--- src/components/zhn-r/RowInputText.tsx
+++ src/components/zhn-r/RowInputText.tsx
@@ -7,29 +7,24 @@ import {
   S_TEXT 
 } from './style';
 
-export interface RowInputType2Props {
-  styleInput?: CSSProperties;
+export interface RowInputTextProps {
   caption: string;
+  styleInput?: CSSProperties;
   initValue?: string;
   onEnter?: (v: string) => void; 
 }
 
-const RowInputType2 = ({
-  styleInput,
-  caption, 
-  initValue,
-  onEnter
-}: RowInputType2Props) => (
+const RowInputText = (props: RowInputTextProps) => (
   <div className={CL_ROW}>     
      <label className={CL_CAPTION}>
-       <span>{caption}</span>
+       <span>{props.caption}</span>
        <InputText
-         style={{...S_TEXT, ...styleInput}}
-         initialValue={initValue}
-         onEnter={onEnter}
+         style={{...S_TEXT, ...props.styleInput}}
+         initialValue={props.initValue}
+         onEnter={props.onEnter}
         />
      </label>     
   </div>
 );
 
-export default RowInputType2
+export default RowInputText
