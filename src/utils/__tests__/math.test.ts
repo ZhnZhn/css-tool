e57d76fe95d17587c3347b3bde8baaca70db46e0
Yevhenii Molodyi <eugmolody@gmail.com>
2026-02-24 14:07:30 +0200

    Add function splitNumberByE to utils math module

--- src/utils/__tests__/math.test.ts
+++ src/utils/__tests__/math.test.ts
@@ -3,26 +3,37 @@ import {
   toPercent
 } from '../math';
 
-describe('math', ()=>{
-    test('round10 should return value by string args', ()=>{
-      expect(round10('10.4444', '0')).toBe(10)
-      expect(round10('10.5555', '0')).toBe(11)
-    })
-    test('round10 should return value by number args', ()=>{
-      expect(round10(10.4444, 0)).toBe(10)
-      expect(round10(10.5555, 0)).toBe(11)
-    })
-    test('round10 should return value by string and number args', ()=>{
-        expect(round10('10.4444', 0)).toBe(10)
-        expect(round10(10.5555, '0')).toBe(11)
-    })
+describe('round10', ()=>{   
+  const fn = round10;
+  test('should return rounded value by string args', ()=>{
+    expect(fn('10.4444', '0')).toBe(10)
+    expect(fn('10.5555', '0')).toBe(11)
+  })
+  test('should return rounded value by number args', ()=>{
+    expect(fn(10.4444, 0)).toBe(10)
+    expect(fn(10.5555, 0)).toBe(11)
+  })
+  test('should return rounded value by string and number args', ()=>{
+    expect(fn('10.4444', 0)).toBe(10)
+    expect(fn(10.5555, '0')).toBe(11)
+  })
+  test('should return rounded value by exponential string args', ()=>{
+    expect(fn('1.04444e+1', 0)).toBe(10)
+    expect(fn('1.04444e+1', '0')).toBe(10)
+    expect(fn('1.05555e+1', 0)).toBe(10)
+    expect(fn('1.05555e+1', '0')).toBe(10)
+  })
 
-    test('toPercent should return value by v, min, max', ()=>{
-      expect(toPercent(1, 0, 10)).toBe(10)  
-      expect(toPercent(5, 0, 10)).toBe(50)
-      expect(toPercent(9, 0, 10)).toBe(90)
-    })
-    test('toPercent should return 0 in edge case: min=max', ()=>{
-      expect(toPercent(10, 10, 10)).toBe(0)  
-    })
+})  
+
+describe('toPercent', ()=>{
+  const fn = toPercent;
+  test('should return value by v, min, max', ()=>{
+    expect(fn(1, 0, 10)).toBe(10)  
+    expect(fn(5, 0, 10)).toBe(50)
+    expect(fn(9, 0, 10)).toBe(90)
+  })
+  test('should return 0 in edge case: min=max', ()=>{
+    expect(fn(10, 10, 10)).toBe(0)  
+  })
 })
--- src/utils/math.ts
+++ src/utils/math.ts
@@ -3,6 +3,10 @@ import {
   isNumber 
 } from "./isTypeFn";
 
+const splitNumberByE = (
+  n: number
+) => ('' + n).split('e');
+
 export const round10 = (
   value: number | string, 
   exp: number | string | null | undefined
@@ -10,17 +14,19 @@ export const round10 = (
   if (exp == null) {
     return NaN;
   }
+
   value = +value;
   exp = +exp;
   // If the value is not a number or the exp is not an integer...
   if (isNaN(value) || !(isNumber(exp) && exp % 1 === 0)) {
     return NaN;
   }
+
   // Shift      
-  let _arrV = value.toString().split('e');
+  let _arrV = splitNumberByE(value);   
   value = Math.round(+(_arrV[0] + 'e' + (_arrV[1] ? (+_arrV[1] - exp) : -exp)));
   // Shift back
-  _arrV = value.toString().split('e');
+  _arrV = splitNumberByE(value)  
   return +(_arrV[0] + 'e' + (_arrV[1] ? (+_arrV[1] + exp) : exp));
 }
 
@@ -29,8 +35,8 @@ export const toPercent = (
   min: number, 
   max: number
 ): number => {
-  const _percent = (value - min ) / (max - min);
-  return _isNaN(_percent)
+  const percent = (value - min) / (max - min);
+  return _isNaN(percent)
     ? 0
-    : _percent*100;
+    : percent * 100;
 }
